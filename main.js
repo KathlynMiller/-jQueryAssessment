4317806fb6f9fe9e4530d7401411a96744ef1455
Kathlyn Miller <katkit101999@hotmail.com>
2015-04-20 13:16:54 -0700

    finished quiz

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('div').click(function(){
+
+		alert(" Hey you clicked me");
+	});
+}); 
+
+$(document).ready(function(){
+   $('#iwaofunction'() {
+      
+
+      alert(" Hey you clicked me again");
+   });
+});
